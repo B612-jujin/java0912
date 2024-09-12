78125cfa74c30457e2abd357ef5ec571d0ef7879
B612-jujin <jujingood@gmail.com>
2024-09-12 10:00:55 +0900

    java0912

---
+++ chp08/ex8_1/Hero.java
@@ -0,0 +1,31 @@
+package chp08.ex8_1;
+
+public class Hero {
+    String name;
+    int power;
+    int speed;
+
+    public Hero(String name){
+        this.name = name;
+    }
+
+    public Hero(String name, int power) {
+        this.name = name;
+        this.power = power;
+    }
+
+    public Hero(String name, int power, int speed) {
+        this.name = name;
+        this.speed = speed;
+        this.power = power;
+    }
+
+    @Override
+    public String toString() {
+        return "Hero{" +
+                "name='" + name + '\'' +
+                ", power=" + power +
+                ", speed=" + speed +
+                '}';
+    }
+}
